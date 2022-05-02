563b61b2a1d5d568862f799e860b46cb063b4eef
mayj <mayj@uoguelph.ca>
2022-05-02 18:36:02 -0400

    Added acknowledgments section.
    Added file.choose lines so user can read in their own datasets. Added optional lines for matching original and complete-case datasets to species in trees (if using phylogenetic imputation).
    
    Removed consideration of NAs from ScreenCategories function.

--- R/Section_1_ImputationPrep.R
+++ R/Section_1_ImputationPrep.R
@@ -1,5 +1,17 @@
 # Section 1. Script for preparing data for imputation process including checking distributions and categorical frequencies.
 
+# Acknowledgments. 
+
+# Trait data used in this study obtained from:
+# Meiri, S. Traits of lizards of the world: Variation around a successful evolutionary design. Global Ecol Biogeogr. 2018; 27: 1168– 1172. https://doi.org/10.1111/geb.12773
+# Meiri, Shai (2019), Data from: Traits of lizards of the world: variation around a successful evolutionary design, Dryad, Dataset, https://doi.org/10.5061/dryad.f6t39kj
+
+# Multiple sequence alignment obtained from:
+# Pyron, R.A., Burbrink, F.T. & Wiens, J.J. A phylogeny and revised classification of Squamata, including 4161 species of lizards and snakes. BMC Evol Biol 13, 93 (2013). https://doi.org/10.1186/1471-2148-13-93
+# Pyron RA, Burbrink FT, Wiens JJ. Data from: A phylogeny and revised classification of Squamata, including 4161 species of lizards and snakes. Dryad Dataset [Internet]. 2013; Available from: https://doi.org/10.5061/dryad.82h0m
+
+# Cleaned COI dataset provided by Dr. Cameron Nugent. Originally downloaded from the Barcode of Life Data (BOLD) System on March 4th, 2020.
+
 ### 1. Load libraries and functions. ----
 
 library(ape)
@@ -10,43 +22,55 @@ source("R/Functions/Phylo_Functions.R")
 
 ### 2. Dataset loading and variable assignment. ----
 
-# Read in the original trait dataset with missing data.
-dfRaw <- fread("Data/TraitData/OriginalDataset.csv", data.table = F)
-# Create vector of column names that contain taxonomic/misc information.
+# Read in trait dataset you wish to impute. For an example, you can download the Meiri (2018) dataset available at the DOI mentioned above.
+fileName <- file.choose()
+dfRaw <- read.csv(fileName)
+# Ensure blanks are NAs.
+dfRaw[dfRaw == ""] <- NA
+# Create vector of column names that contain taxonomic/misc information. For example:
 taxCols <- "species_name"
-# Extract trait names for this taxon.
+# Extract trait names for this taxon (everything other than taxCols). I.e. the traits you want to consider for imputation.
 traits <- setdiff(colnames(dfRaw), taxCols)
 
-# Complete-case dataset creation. ---
-# If already created, read in the complete-case trait dataset.
-dfCC <- fread("Data/TraitData/CompleteCaseDataset.csv", data.table = F)
-# OPTIONAL:
-# Let's create candidate complete-case datasets.
-# Apply the CritNumberLoop() function to dfRaw to create a list of potential complete-case dataframes that contain data for at least three traits. This function writes the candidate complete-case dataframes to file. Uncomment the following line if you wish to use this function:
-CritNumberLoop(data = dfRaw, traitCols = traits, taxCols = "species_name")
-# Once selected, read in the complete-case dataset by uncommenting the following line:
+# Complete-case dataset. ---
+# If already created, read in complete-case trait dataset.
 fileName <- file.choose()
 dfCC <- read.csv(fileName)
+# Ensure blanks are NAs.
+dfCC[dfCC == ""] <- NA
 
-# If using phylogenetic imputation, read in tree(s).
-treeFiles <- list.files(path = "Data/RAxMLTrees/", pattern = "_ultra.tre")
-l_trees <- lapply(treeFiles, function(x) read.tree(paste("Data/RAxMLTrees/", x, sep = "")))
-# Name l_trees.
-names(l_trees) <- treeFiles
-# Match species names in trees to those in complete-case dataset. If less than 100, consider dropping traits here.
-l_matched <- lapply(l_trees, DropAndMatch, data = dfCC)
-# Extract updated trees (species not found in complete-case dataset were dropped from trees).
-l_trees <- lapply(l_matched, function(x) x[[1]])
-# Check number of species in the trees.
-sapply(l_trees, function(x) length(x$tip.label)) ## Greater than 100? If not, consider excluding tree from imputation simulations.
+# OPTIONAL: Complete-case data-set creation. ---
+# If you don't want to allow for any missingness in your complete-case dataset:
+# Apply the CritNumberLoop() function to dfRaw to create a list of potential complete-case dataframes that contain data for at least three traits. This function writes the candidate complete-case dataframes to file.
+# Uncomment the following lines if you wish to use this function:
+#ns <- seq(100, 5000, by = 100) ## These are the sample size thresholds to consider for your complete-case datasets.
+#CritNumberLoop(data = dfRaw, traitCols = traits, taxCols = "species_name", critNumbers = ns)
+# Once selected, read in the complete-case dataset by uncommenting the following line:
+#fileName <- file.choose()
+#dfCC <- read.csv(fileName)
 
 # Combine complete-case and original data into list.
 l_dfTraits <- list(Complete = dfCC, Original = dfRaw)
 
+# OPTIONAL: If using phylogenetic imputation, uncomment the following lines.
+# Read in tree. For example:
+#tree <- read.tree("Data/RAxMLTrees/FinalTrees/Squamata_CMOS_ultraTree_finalImp.tre")
+# Match species names in tree to those in datasets. If sample size is less than 100, consider dropping traits here.
+#l_matched <- mapply(DropAndMatch, data = l_dfTraits, MoreArgs = list(tree = tree), SIMPLIFY = F) 
+# Extract updated trees (species not found in datasets were dropped).
+#l_trees <- lapply(l_matched, function(x) x[[1]])
+# Extract updated datasets.
+#l_dfTraits <- lapply(l_matched, function(x) x[[2]])
+
+# Check sample sizes before proceeding!
+lapply(l_dfTraits, nrow)
+
+# Another option here is to allow for some missingness in your complete-case dataset (i.e. a "nearly" complete-case dataset - see Optional_NearlyCompleteCaseCreation.R script if you wish to do this).
+
 ### 3. Check trait data. ----
 
 # Apply BreakIntoTypes() function to identify which traits are numerical and which are categorical.
-l_traits <- BreakIntoTypes(dfCC, traits)
+l_traits <- BreakIntoTypes(l_dfTraits[[1]], traits)
 # Extract numerical traits.
 contTraits <- l_traits[[1]]
 # Extract categorical traits.
@@ -101,16 +125,16 @@ names(l_dfOutliers) <- contTraits
 View(l_dfOutliers$smallest_clutch$Complete)
 
 # Check for class imbalances in the complete-case data.
-catFreqsCC <- lapply(dfCC[, catTraits], ScreenCategories)
+catFreqsCC <- lapply(na.omit(l_dfTraits$Complete[, catTraits]), ScreenCategories)
 print(catFreqsCC)
 # Check for class imbalances in the original data.
-catFreqsO <- lapply(dfRaw[, catTraits], ScreenCategories)
+catFreqsO <- lapply(na.omit(l_dfTraits$Original[, catTraits]), ScreenCategories)
 print(catFreqsO)
 
-# At this point, you would remove categories that fall below the indicated threshold (e.g. comprising less than 10% of the data).
+# At this point, you would remove categories that fall below the indicated threshold (e.g. comprising less than 10% of the data). If you remove observations here, it is recommended to run the ScreenCategories function again to ensure classes are balanced.
 # Example:
-dfRaw <- dfRaw[!dfRaw$insular_endemic == "unknown", ]
+l_dfTraits$Original <- l_dfTraits$Original[!l_dfTraits$Original$insular_endemic == "unknown", ]
 
-# If any data were removed, write the cleaned data to file for use in imputation simulations. No alterations to complete-case dataset so we can leave it as is.
-#fwrite(dfCC, "Data/TraitData/CleanedCompleteCaseDataset.csv")
-#fwrite(dfRaw, "Data/TraitData/CleanedOriginalCaseDataset.csv")
+# If any data were removed, write the cleaned data to file for use in imputation simulations.
+#fwrite(l_dfTraits$Complete, "Data/TraitData/CleanedCompleteCaseDataset.csv")
+#fwrite(l_dfTraits$Original, "Data/TraitData/CleanedOriginalCaseDataset.csv")
